1a60d9e7f7f97f3ecd65203d2a9ddadeee696755
julezKiano <jgkiano@gmail.com>
2019-08-26 18:40:21 +0300

    added SkewedBackground and PostList to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>Hello Vue</div>
+  <div class="home-component-container relative">
+    <SkewedBackground />
+    <div class="container mx-auto relative z-10 p-8">
+      <PostList />
+    </div>
+  </div>
 </template>
+
+<script>
+import SkewedBackground from '../components/SkewedBackground.vue';
+import PostList from '../components/PostList.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    SkewedBackground,
+    PostList,
+  },
+};
+</script>
