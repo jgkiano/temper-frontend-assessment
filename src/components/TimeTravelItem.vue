7d0bfe2267d5e5ca1b3fda5de4b749012c782a63
julezKiano <jgkiano@gmail.com>
2019-08-26 19:55:42 +0300

    created skeleton TimeTravelItem component

---
+++ src/components/TimeTravelItem.vue
@@ -0,0 +1,61 @@
+<template>
+  <div
+    class="flex flex-row items-center p-2"
+    :class="showBottomBorder ? 'border-b border-gray-300' : ''"
+  >
+    <div
+      class="flex-grow text-sm text-gray-700"
+    >Moved post {{ postNumber }} from index {{ fromIndex }} to index {{ toIndex }}</div>
+    <div>
+      <button
+        @click="handleButtonClick"
+        class="time-travel-btn text-grey font-semibold py-3 px-10 rounded text-sm"
+      >Time travel</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    postNumber: {
+      type: Number,
+      required: true,
+    },
+    fromIndex: {
+      type: Number,
+      required: true,
+    },
+    toIndex: {
+      type: Number,
+      required: true,
+    },
+    showBottomBorder: {
+      type: Boolean,
+      default: true,
+    },
+    index: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    handleButtonClick() {
+      this.$emit('buttonClicked', this.index);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scopped>
+.time-travel-btn {
+  background-color: #25fa8f;
+  transition: background ease-in-out 0.3s;
+  &:hover {
+    background-color: darken(#25fa8f, 10%);
+  }
+  &:focus {
+    outline: 0;
+  }
+}
+</style>
