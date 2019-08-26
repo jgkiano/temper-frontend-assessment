c33482bf9ece7abf955c86256fd0d60369f6a2e5
julezKiano <jgkiano@gmail.com>
2019-08-26 21:53:40 +0300

    change list order

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="lg:w-1/2 px-6">
     <div class="text-3xl font-semibold text-white lg:pb-4 lg:py-0 py-4">Sortable Post List</div>
-    <PostItem :index="0" title="Post 1" @moveUp="onMoveUp" @moveDown="onMoveDown" arrows="up" />
+    <PostItem :index="0" title="Post 1" @moveUp="onMoveUp" @moveDown="onMoveDown" arrows="down" />
     <PostItem :index="1" title="Post 2" @moveUp="onMoveUp" @moveDown="onMoveDown" />
-    <PostItem :index="2" title="Post 3" @moveUp="onMoveUp" @moveDown="onMoveDown" arrows="down" />
+    <PostItem :index="2" title="Post 3" @moveUp="onMoveUp" @moveDown="onMoveDown" arrows="up" />
   </div>
 </template>
 
