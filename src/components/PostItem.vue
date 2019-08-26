f4e59e4e48c113c575ea18f46503d0f8e42f6a49
julezKiano <jgkiano@gmail.com>
2019-08-26 19:54:34 +0300

    reduced padding

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,7 +1,7 @@
 // TODO: find a more effective way of implementing chevron icons
 <template>
   <div
-    class="bg-white rounded shadow-md flex flex-row items-center justify-center h-20 px-6 py-4 mb-6"
+    class="bg-white rounded shadow-md flex flex-row items-center justify-center h-20 px-4 py-4 mb-6"
   >
     <div class="text-gray-700 flex-grow">{{ title }}</div>
     <div
