127ad81ece1f85d5534f217378cb30649dce16a6
julezKiano <jgkiano@gmail.com>
2019-08-26 23:13:22 +0300

    added vue.use

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
+import Vue from 'vue';
 import Vuex from 'vuex';
-import posts from './posts.module';
+import post from './post.module';
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: { posts },
+  modules: { post },
 });
