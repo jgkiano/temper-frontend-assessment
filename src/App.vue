16d9f4e9c26f229c839df8234ce10515ef833dbc
julezKiano <jgkiano@gmail.com>
2019-08-26 23:12:58 +0300

    fetch posts at app created

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,16 @@
     <router-view />
   </div>
 </template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  created() {
+    this.fetchPosts();
+  },
+  methods: {
+    ...mapActions('post', ['fetchPosts']),
+  },
+};
+</script>
