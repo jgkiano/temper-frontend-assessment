744031b22aa29d5b33198cc6d1bc97479e2b5662
julezKiano <jgkiano@gmail.com>
2019-08-26 20:16:29 +0300

    made time travel button responsive

--- src/components/TimeTravelItem.vue
+++ src/components/TimeTravelItem.vue
@@ -1,15 +1,15 @@
 <template>
   <div
-    class="flex flex-row items-center p-2"
+    class="flex flex-row items-center p-2 items-center justify-center"
     :class="showBottomBorder ? 'border-b border-gray-300' : ''"
   >
     <div
-      class="flex-grow text-sm text-gray-700"
+      class="flex-grow text-sm text-gray-700 lg:p-0 p-2"
     >Moved post {{ postNumber }} from index {{ fromIndex }} to index {{ toIndex }}</div>
     <div>
       <button
         @click="handleButtonClick"
-        class="time-travel-btn text-grey font-semibold py-3 px-10 rounded text-sm sm:text-xs"
+        class="time-travel-btn text-grey font-semibold lg:py-3 lg:px-10 py-2 px-4 rounded text-sm"
       >Time travel</button>
     </div>
   </div>
