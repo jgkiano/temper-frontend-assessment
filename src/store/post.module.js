206d81538cef53e1b8f30eca3794d601d66605f1
julezKiano <jgkiano@gmail.com>
2019-08-26 23:16:50 +0300

    added fetchPosts action and setPosts mutation

---
+++ src/store/post.module.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const MAX_POSTS = 5;
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com/posts?_limit=';
+
+const initialState = {
+  posts: [],
+};
+
+const actions = {
+  fetchPosts: async ({ commit }, limit = MAX_POSTS) => {
+    try {
+      const { data } = await axios.get(`${BASE_URL}${limit}`);
+      commit('setPosts', data);
+    } catch (error) {
+      // TODO: handle errors
+      console.error(error);
+    }
+  },
+};
+
+const mutations = {
+  setPosts: (state, posts) => {
+    state.posts = posts;
+  },
+};
+
+export default {
+  namespaced: true,
+  state: initialState,
+  actions,
+  mutations,
+};
