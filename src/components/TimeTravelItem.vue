bdf128025e3e701994bd2bfaf0374be5398b8b52
julezKiano <jgkiano@gmail.com>
2019-08-27 23:56:33 +0300

    moved emit to template

--- src/components/TimeTravelItem.vue
+++ src/components/TimeTravelItem.vue
@@ -8,7 +8,7 @@
     >Moved post {{ id }} from index {{ from }} to index {{ to }}</div>
     <div>
       <button
-        @click="handleButtonClick"
+        @click="$emit('buttonClicked')"
         class="time-travel-btn text-grey font-semibold lg:py-3 lg:px-10 py-2 px-4 rounded text-sm"
       >Time travel</button>
     </div>
@@ -33,11 +33,6 @@ export default {
     hideBottomBorder: {
       type: Boolean,
       default: false,
-    },
-  },
-  methods: {
-    handleButtonClick() {
-      this.$emit('buttonClicked');
     },
   },
 };
