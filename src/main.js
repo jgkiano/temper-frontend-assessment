8c10dbcedd0edcdb649d0078bfda429a6ba3acc0
julezKiano <jgkiano@gmail.com>
2019-08-27 13:43:41 +0300

    added fortawesome lib implementation

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './assets/css/main.scss';
+
+library.add(faChevronDown, faChevronUp);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
