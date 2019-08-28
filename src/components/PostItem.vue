c24a29d3d7a11853a1ffff714aad7370a3a9fff4
julezKiano <jgkiano@gmail.com>
2019-08-28 18:52:04 +0300

    wrapped icons with div, made title not mandatory

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -2,15 +2,23 @@
   <div
     class="post-item-container bg-white rounded shadow-md flex flex-row justify-center px-4 py-4 mb-6"
   >
-    <div class="text-gray-700 flex-grow flex items-center">{{ postTitle }}</div>
+    <div class="post-item-title text-gray-700 flex-grow flex items-center">{{ postTitle }}</div>
     <div class="flex flex-col" :class=" arrows === 'both' ? 'justify-between' : 'justify-center'">
-      <div class="up-arrow-container" v-if="arrows !== 'down'" @click="$emit('moveUp')">
+      <div
+        class="arrow-container up-arrow-container"
+        v-if="arrows !== 'down'"
+        @click="$emit('moveUp')"
+      >
         <font-awesome-icon
           icon="chevron-up"
           class="block cursor-pointer text-indigo-800 hover:text-indigo-600"
         />
       </div>
-      <div class="down-arrow-container" v-if="arrows !== 'up'" @click="$emit('moveDown')">
+      <div
+        class="arrow-container down-arrow-container"
+        v-if="arrows !== 'up'"
+        @click="$emit('moveDown')"
+      >
         <font-awesome-icon
           icon="chevron-down"
           class="block cursor-pointer text-indigo-800 hover:text-indigo-600"
@@ -24,7 +32,10 @@
 export default {
   name: 'PostItem',
   props: {
-    title: String,
+    title: {
+      type: String,
+      default: '',
+    },
     arrows: {
       default: 'both',
       type: String,
